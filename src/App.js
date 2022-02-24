50a692ab9010e6792ad65e9d0e3dd10df738b06f
ohhyunji <helloqu@naver.com>
2022-02-24 17:16:03 +0900

    상품업로드 페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
+import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
 import './App.css';
 import MainPageComponent from "./main/index.js"
 import React from 'react'
+import {Button} from 'antd'
+import {DownloadOutlined} from '@ant-design/icons'
 
 //페이지 이동 라우터 관련
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route,Link,useHistory} from 'react-router-dom'
 import UploadPage from './upload';
 import ProductPage from './product';
 
 function App() {
+  const history = useHistory();
   return (
       <div>
         <div id="header">
           <div id="header-area">
-              <img src="../images/images/icons/logo.png" alt=""/>
+            <Link to={"/"}>
+            <img src="../images/images/icons/logo.png" alt=""/>
+            </Link>
+            <Button size='large' onClick={function(){
+              // alert('업로드클릭')
+              history.push("/upload");
+            }} icon={<DownloadOutlined/>}>상품업로드</Button>
           </div>
         </div>
         <div id="body">
